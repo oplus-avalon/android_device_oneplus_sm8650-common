a558768c10a78f943b103a5e415e7142c7eb0ca2
K R I S H N A <krishnakripa34567@gmail.com>
2026-06-13 09:45:30 +0000

    sm8650-common: Drop `excluded-input-devices` vibrator config
    * no longer needed as we have shifted to prebuilt libs.
    
    Change-Id: I6edc44400bdb94ad72ec911e6f6771ddb36f2b02
    Signed-off-by: K R I S H N A <krishnakripa34567@gmail.com>

--- common.mk
+++ common.mk
@@ -469,12 +469,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
-# Vibrator
-ifneq ($(TARGET_IS_TABLET),true)
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
-endif
-
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/oplus/vintf/device_framework_matrix.xml \
